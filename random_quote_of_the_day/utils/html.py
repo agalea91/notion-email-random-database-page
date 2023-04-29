90cb3169c502c8c8d7f9d463331e8776876d79bb
Alexander Galea <agalea91@gmail.com>
2023-04-29 09:30:38 -0400

    Make quote pretty

--- random_quote_of_the_day/utils/html.py
+++ random_quote_of_the_day/utils/html.py
@@ -1,54 +1,5 @@
-STYLE = """\
-        .styled-table {
-            border-collapse: collapse;
-            margin: 25px 0;
-            font-size: 0.9em;
-            font-family: sans-serif;
-            min-width: 400px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-        }
-
-        .styled-table thead tr {
-            background-color: #009879;
-            color: #ffffff;
-            text-align: left;
-        }
-
-        .styled-table th,
-        .styled-table td {
-            padding: 12px 15px;
-        }
-
-        .styled-table tbody tr {
-            border-bottom: 1px solid #dddddd;
-        }
-
-        .styled-table tbody tr:nth-of-type(even) {
-            background-color: #f3f3f3;
-        }
-
-        .styled-table tbody tr:last-of-type {
-            border-bottom: 2px solid #009879;
-        }
-
-        .styled-table tbody tr.active-row {
-            font-weight: bold;
-            color: #009879;
-        }
-
-        h1 {
-            color: #009879;
-            font-family: arial, sans-serif;
-            font-size: 16px;
-            font-weight: bold;
-            margin-top: 0px;
-            margin-bottom: 1px;
-        }
-"""
-
-
 def quote_html(quote, author):
-    quote_html = f"""\
+    return f"""
 <div>
     <blockquote>
         <p>{quote}</p>
@@ -56,19 +7,26 @@ def quote_html(quote, author):
     </blockquote>
 </div>
 """
-    return quote_html
+
+
+def pretty_quote_html(quote, author):
+    return f"""
+    <figure style='margin: 0; background: #eee; padding: 1em; border-radius: 1em;'>
+        <blockquote style='margin: 1em;'>
+            {quote}
+        </blockquote>
+        <figcaption style='margin: 1em;'>
+            <cite>{author}</cite>
+        </figcaption>
+    </figure>
+    """
 
 
 def construct_html_msg(quote, attrib):
     html_template = f"""\
 <html>
-    <head>
-    <style>
-        {STYLE}
-    </style>
-    </head>
-    <body>
-        {quote_html(quote, attrib)}
+    <body style='margin: 1em; font: 1.2rem/1.4 Georgia, serif;'>
+        {pretty_quote_html(quote, attrib)}
     </body>
 </html>
 """
